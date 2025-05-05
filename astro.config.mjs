7aada3d5d1d00a941e6ab87b3231736ffbfe7182
polycaon <history.careers@gmail.com>
2025-05-05 12:02:17 +0530

    Commit from Bolt to GitHub

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,26 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import vercel from '@astrojs/vercel/static';
 import prefetch from '@astrojs/prefetch';
+import sitemap from '@astrojs/sitemap';
+import robotsTxt from 'astro-robots-txt';
 
 export default defineConfig({
-  integrations: [tailwind(), prefetch()],
+  site: 'https://brihadeeswaratemple.com',
+  integrations: [
+    tailwind(), 
+    prefetch(),
+    sitemap(),
+    robotsTxt({
+      policy: [
+        {
+          userAgent: '*',
+          allow: '/',
+          disallow: ['/private', '/admin'],
+        },
+      ],
+      sitemap: true,
+    })
+  ],
   output: 'static',
   adapter: vercel({
     webAnalytics: {
